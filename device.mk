a2ce721d8d63d48a775a1001704463e97e3fade6
sweetyicecare <gabrielseedev@outlook.com>
2020-02-26 06:37:15 +0000

    santoni: Build QCOM powerHAL

--- device.mk
+++ device.mk
@@ -368,8 +368,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    power.qcom \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
