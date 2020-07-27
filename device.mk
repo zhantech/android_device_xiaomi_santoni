e80fef5935e58463a4b47c7464f67d7af8666cc1
Foxtrot47 <jjneutron@outlook.com>
2020-07-27 14:45:28 +0000

    santoni: Update PRODUCT_DEXPREOPT_SPEED_APPS

--- device.mk
+++ device.mk
@@ -179,8 +179,7 @@ include $(LOCAL_PATH)/rootdir/charger/charger.mk
 
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI \
-    NexusLauncherRelease
+    SystemUI
 
 # Display
 PRODUCT_PACKAGES += \
