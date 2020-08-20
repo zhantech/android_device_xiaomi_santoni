2caf06f4104399cad32d7dfa62640401ce604301
zhantech <hifzhan41@gmail.com>
2020-08-20 08:48:18 +0000

    santoni: Remove Some Default Apps

--- Google/Android.mk
+++ Google/Android.mk
@@ -26,7 +26,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX Jelly GoogleTTS talkback PixelLiveWallpaperPrebuilt ViaBrowser ChromeModernPublic Browser Browser2 Jelly DuckDuckGo Chrome
+LOCAL_OVERRIDES_PACKAGES := AudioFX Jelly GoogleTTS talkback PixelLiveWallpaperPrebuilt ViaBrowser ChromeModernPublic Browser Browser2 Jelly DuckDuckGo Chrome Email Eleven Music
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
