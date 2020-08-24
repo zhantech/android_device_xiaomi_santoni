4c36113ee856be54668f460173054baaf97dc865
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-08-24 01:56:19 +0000

    santoni: Bringup Nusantara

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -29,9 +29,9 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-AOSP_ROOT="${MY_DIR}/../../.."
+NAD_ROOT="${MY_DIR}/../../.."
 
-HELPER="$AOSP_ROOT/vendor/havoc/build/tools/extract_utils.sh"
+HELPER="$NAD_ROOT/vendor/nad/build/tools/extract_utils.sh"
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -39,7 +39,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${AOSP_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${NAD_ROOT}"
 
 # Copyright headers and guards
 write_headers
