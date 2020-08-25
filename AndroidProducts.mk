b5b76166514b7247b85ab1b6d4ba0b680a9fb8ac
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-08-25 08:09:54 +0000

    santoni: Rebase Komodo-Os

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_santoni.mk
+    $(LOCAL_DIR)/komodo_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_santoni-userdebug \
-    havoc_santoni-user \
-    havoc_santoni-eng
+    komodo_santoni-userdebug \
+    komodo_santoni-user \
+    komodo_santoni-eng
