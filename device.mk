1feb03c76fb01fd0bfc3b8fa16f0533a7c72dc39
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-08-30 04:36:59 +0000

    Revert "santoni: add prebuilts kimcirecorder"
    This reverts commit 02b3f05edb3dcac6093883af79a0527120fd6500.

--- device.mk
+++ device.mk
@@ -370,10 +370,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
-# Recorder
-PRODUCT_PACKAGES += \
-    KimciRecorder
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson
