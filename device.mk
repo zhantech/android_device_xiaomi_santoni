9f4c5b33aff016c467ec04b94b86036edfa101ac
Stargazer19 <pviraj19@gmail.com>
2020-09-13 05:19:04 +0530

    santoni: Add vendor version of libbacktrace and libunwind
    * Also Compile libstdc++.vendor

--- device.mk
+++ device.mk
@@ -219,7 +219,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1 \
-    libunwind
+    libbacktrace-vendor \
+    libunwind-vendor
 
 # FM
 PRODUCT_PACKAGES += \
@@ -470,6 +471,7 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-ext \
+    libstdc++.vendor
 
 # Wifi
 PRODUCT_PACKAGES += \
