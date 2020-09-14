8f43118d9d42a27dfb465f3a65956e05facb14ac
zhantech <hifzhan41@gmail.com>
2020-09-14 09:57:39 +0000

    santoni: Remove Camera Go, Change Default Camera

--- device.mk
+++ device.mk
@@ -156,7 +156,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8937
+    camera.msm8937 \
+    Snap
 
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
@@ -498,13 +499,13 @@ PRODUCT_PACKAGES += \
 #XiaomiAddon
 PRODUCT_PACKAGES += \
     XiaomiAddon
-
+	
 #XiaomiParts
 PRODUCT_PACKAGES += \
     XiaomiParts
 
-# Google
+# Bloatware
 PRODUCT_PACKAGES += \
-    GCam \
     GalleryGo \
     RemovePackages
+
