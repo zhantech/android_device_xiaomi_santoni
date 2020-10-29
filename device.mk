d6ee270e0b1db8ec5383ff2881d395756c6ca38d
zhantech <hifzhan41@gmail.com>
2020-10-29 03:03:24 +0000

    santoni: add NusantaraPapers

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/msm8996/media
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+PRODUCT_PACKAGES += \
+    NusantaraPapers
+
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
