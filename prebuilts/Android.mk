0f32a11d01a76bdc5fe2ab7e833d53c9ec763f06
zhantech <hifzhan41@gmail.com>
2021-02-25 02:48:08 +0000

    santoni: Remove Trebuchet and Updater

--- prebuilts/Android.mk
+++ prebuilts/Android.mk
@@ -31,7 +31,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX Launcher3QuickStep
+LOCAL_OVERRIDES_PACKAGES := AudioFX Launcher3QuickStep TrebuchetQuickStep Trebuchet Updater
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
